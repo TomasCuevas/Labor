dbb0e5a6604a2e7d5e1e94f646d4b990a67bf974
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-17 14:40:31 -0300

    feat: :sparkles: add connection to database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl:
+        process.env.STAGE === 'prod'
+          ? {
+              rejectUnauthorized: false,
+              sslmode: 'require',
+            }
+          : (false as any),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
