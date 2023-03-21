0ac657c8f024c4ad8d2782bc500f97c5eee87a32
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-21 12:39:52 -0300

    feat(server): :sparkles: add decorator to get a user from request

---
+++ packages/server/src/auth/decorators/get-user.decorator.ts
@@ -0,0 +1,20 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (field: string, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user) {
+      throw new InternalServerErrorException(
+        'Usuario no encontrado. Contacte con un administrador.',
+      );
+    }
+
+    return field ? user[field] : user;
+  },
+);
