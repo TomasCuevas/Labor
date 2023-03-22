f73d1dc00ebb539d559ac6af40a2a5beca93b28d
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-22 14:14:47 -0300

    feat(client): :sparkles: add Auth services

---
+++ packages/client/services/auth/auth.services.ts
@@ -0,0 +1,58 @@
+//* axios *//
+import { authApi } from "../../api";
+
+//* interfaces *//
+import { IRegister, ILogin } from "../../interfaces";
+
+//! register service
+export const registerService = async (registerData: IRegister) => {
+  try {
+    const { data } = await authApi.post("/register", registerData);
+
+    return {
+      ok: true,
+      data,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return {
+      ok: false,
+      message: error.response.data.message[0],
+    };
+  }
+};
+
+//! login service
+export const loginService = async (loginData: ILogin) => {
+  try {
+    const { data } = await authApi.post("/login", loginData);
+
+    return {
+      ok: true,
+      data,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return {
+      ok: false,
+      message: error.response.data.message[0],
+    };
+  }
+};
+
+//! check service
+export const checkService = async () => {
+  try {
+    const { data } = await authApi.get("/check");
+
+    return {
+      ok: true,
+      data,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+    };
+  }
+};
