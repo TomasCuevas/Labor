ebf27c5e7858ab8de8d903d4fa4232af721804e2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-22 16:35:29 -0300

    refactor(client): :recycle: move layouts folder out of components folder

---
+++ packages/client/layouts/AuthLayout.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+//* components *//
+import { FullLoader } from "../components/ui";
+
+//* context *//
+import { AuthContext } from "../context";
+
+//* interface *//
+interface Props {
+  children: React.ReactNode;
+  description: string;
+  title: string;
+}
+
+export const AuthLayout: React.FC<Props> = ({
+  children,
+  description,
+  title,
+}) => {
+  const { status } = useContext(AuthContext);
+  const router = useRouter();
+
+  if (status === "authenticated") router.replace("/");
+  if (status === "not-authenticated") {
+    return (
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+        </Head>
+        <main className="flex h-screen bg-dark p-4">{children}</main>
+      </>
+    );
+  }
+
+  return <FullLoader />;
+};
