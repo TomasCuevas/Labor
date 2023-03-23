2470b5f568fc59a4f099664b41c2d43cf7eb88ca
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-23 16:01:12 -0300

    feat(server): :sparkles: add new validations on create-user-dto

--- packages/server/src/users/dto/create-user.dto.ts
+++ packages/server/src/users/dto/create-user.dto.ts
@@ -1,24 +1,28 @@
-import {
-  IsEmail,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail({}, { message: 'El email debe tener un formato valido.' })
   email: string;
 
-  @IsString({ message: 'El nombre solo puede contener letras.' })
+  @Matches(/^[a-zA-ZÀ-ÖØ-öø-ſ]+([ '-][a-zA-ZÀ-ÖØ-öø-ſ]+)*$/, {
+    message:
+      'El nombre solo puede contener letras y espacios, guiones o apóstrofes entre los nombres.',
+  })
   @MinLength(1, { message: 'El nombre debe tener mínimo 1 careacter.' })
   @MaxLength(46, { message: 'El nombre debe tener maximo 46 caracteres.' })
   name: string;
 
   @MinLength(6, { message: 'La contraseña debe tener mínimo 6 caracteres.' })
   @MaxLength(50, { message: 'La contraseña debe tener maximo 50 caracteres.' })
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'La contraseña debe tener mayusculas, minusculas y numeros.',
+  @Matches(/^(?=.*[A-Z])/, {
+    message: 'La contraseña debe tener al menos una mayuscula.',
+  })
+  @Matches(/^(?=.*\d)/, {
+    message: 'La contraseña debe tener al menos un numero.',
+  })
+  @Matches(/^[a-zA-Z0-9\-\.]+$/, {
+    message:
+      'La contraseña solo puede tener letras, numeros, guiones y puntos.',
   })
   password: string;
 }
