9e580da190e5de3b1a537cb826d25f0798df9b8a
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-27 16:10:55 -0300

    refactor(client): :recycle: refactor on boardsApi and authApi

--- packages/client/api/boardsApi.ts
+++ packages/client/api/boardsApi.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const token = Cookies.get("labortoken") || "";
-axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
 const boardsApi = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/boards`,
+});
+
+boardsApi.interceptors.request.use((config) => {
+  config.headers!["Authorization"] = `Bearer ${Cookies.get("labortoken")}`;
+  return config;
 });
 
 export default boardsApi;
