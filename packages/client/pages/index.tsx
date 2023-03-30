450d8ddc5c3256bf630790607bbdfa4c78002dc9
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-30 12:22:39 -0300

    feat(client): :sparkles: add index page

---
+++ packages/client/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+
+//* layout *//
+import { MainLayout } from "../layouts";
+
+//* components *//
+import { YourBoards } from "../components/boards";
+
+const HomePage: NextPage = () => {
+  return (
+    <MainLayout
+      title="Tableros | Labor"
+      description="Página principal de Labor"
+    >
+      <YourBoards />
+    </MainLayout>
+  );
+};
+
+export default HomePage;
