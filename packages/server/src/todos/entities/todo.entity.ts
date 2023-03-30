1885ae5cffead899991e33044fff6315cd22067f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-03-30 16:51:57 -0300

    feat(server): :sparkles: add todo entity

---
+++ packages/server/src/todos/entities/todo.entity.ts
@@ -0,0 +1,51 @@
+import {
+  BeforeUpdate,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+//* entities *//
+import { Board } from '../../boards/entities';
+import { User } from '../../users/entities';
+
+@Entity('todos')
+export class Todo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.id, { eager: true })
+  user: User;
+
+  @ManyToOne(() => Board, (board) => board.id, { eager: true })
+  board: Board;
+
+  @Column({
+    type: 'text',
+  })
+  title: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+    default: 'pending',
+  })
+  status: 'pending' | 'in-progress' | 'completed';
+
+  @Column({
+    type: 'numeric',
+    default: new Date().getTime(),
+  })
+  lastUpdate: number;
+
+  // @BeforeUpdate()
+  // updateLastUpdate() {
+  //   this.lastUpdate = new Date().getTime();
+  // }
+}
