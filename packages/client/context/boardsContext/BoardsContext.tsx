e55ac9c5c92941fb119b64056d0c63f1e6f2eaaf
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-01 16:32:56 -0300

    refactor(client): :recycle: refactor on BoardsContext

--- packages/client/context/boardsContext/BoardsContext.tsx
+++ packages/client/context/boardsContext/BoardsContext.tsx
@@ -1,17 +1,12 @@
 import { createContext } from "react";
-import { useQuery } from "@tanstack/react-query";
-
-//* interfaces *//
-import { IBoard } from "../../interfaces";
+import { useQueryClient } from "@tanstack/react-query";
 
 //* services *//
-import { createBoardService, getAllBoardsService } from "../../services";
+import { createBoardService } from "../../services";
 
 //* CONTEXT *//
 //* CONTEXT *//
 interface BoardsContextProps {
-  boards?: IBoard[];
-  isLoading: boolean;
   onCreateBoard(name: string): Promise<boolean>;
 }
 
@@ -24,18 +19,14 @@ interface BoardsProviderProps {
 }
 
 export const BoardsProvider: React.FC<BoardsProviderProps> = ({ children }) => {
-  const {
-    data: boards,
-    isLoading,
-    refetch,
-  } = useQuery(["boards"], () => getAllBoardsService());
+  const queryClient = useQueryClient();
 
   //! create board
-  const onCreateBoard = async (name: string) => {
+  const onCreateBoard = async (name: string): Promise<boolean> => {
     const result = await createBoardService(name);
 
     if (result.ok) {
-      refetch();
+      queryClient.invalidateQueries(["/boards"]);
       return true;
     } else {
       return false;
@@ -45,10 +36,6 @@ export const BoardsProvider: React.FC<BoardsProviderProps> = ({ children }) => {
   return (
     <BoardsContext.Provider
       value={{
-        //? getters
-        boards,
-        isLoading,
-
         //? methods
         onCreateBoard,
       }}
