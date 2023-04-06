6e59d890da01bfd2e0644811190a2e2f81f7a857
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-05 21:40:20 -0300

    feat(client): :sparkles: add index file on popovers folder

---
+++ packages/client/components/popovers/index.ts
@@ -0,0 +1,7 @@
+export * from "./CreateMenuPopover";
+export * from "./AccountMenuPopover";
+export * from "./items/PopoverCreateItem";
+export * from "./items/PopoverItem";
+export * from "./create/CreateBoard";
+export * from "./form/PopoverInput";
+export * from "./form/PopoverButton";
