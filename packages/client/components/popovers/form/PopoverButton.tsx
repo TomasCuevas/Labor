194f8a5f1e66456cea4d8341b472a644cc411d0f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-05 21:39:03 -0300

    feat(client): :sparkles: add PopoverButton component

---
+++ packages/client/components/popovers/form/PopoverButton.tsx
@@ -0,0 +1,27 @@
+//* interface *//
+interface Props {
+  isDisabled: boolean;
+  label: string;
+  type: "button" | "submit" | "reset";
+  onClick?: any;
+}
+
+export const PopoverButton: React.FC<Props> = ({
+  isDisabled,
+  label,
+  type,
+  onClick,
+}) => {
+  return (
+    <button
+      type={type}
+      disabled={isDisabled}
+      onClick={() => {
+        onClick ? onClick() : null;
+      }}
+      className="cursor-pointer rounded-md bg-emerald py-1 text-sm text-white hover:bg-emerald/80"
+    >
+      {label}
+    </button>
+  );
+};
