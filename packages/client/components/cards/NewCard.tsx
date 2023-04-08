b3b9e491c0692726c2d876c12305b18d6882e86e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-08 19:14:57 -0300

    feat(client): :sparkles: add NewCard component

---
+++ packages/client/components/cards/NewCard.tsx
@@ -0,0 +1,75 @@
+import { MouseEvent, useContext, useState } from "react";
+
+//* icons *//
+import { RiAddLine, RiCloseFill } from "react-icons/ri";
+
+//* hooks *//
+import { useForm } from "../../hooks";
+
+//* context *//
+import { CardContext } from "../../context";
+
+//* interface *//
+import { ICardStatus } from "../../interfaces";
+
+interface Props {
+  status: ICardStatus;
+  boardId: string;
+}
+
+export const NewCard: React.FC<Props> = ({ boardId, status }) => {
+  const { onCreateCard } = useContext(CardContext);
+  const [isInputOpen, setIsInputOpen] = useState<boolean>(false);
+  const { title, onInputChange, reset } = useForm({
+    title: "",
+  });
+
+  const startCreateTodo = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    await onCreateCard({ title, status }, boardId);
+    setIsInputOpen(false);
+    reset();
+  };
+
+  if (isInputOpen) {
+    return (
+      <div>
+        <div>
+          <textarea
+            name="title"
+            value={title}
+            onChange={onInputChange}
+            className="h-20 w-full resize-none rounded-md bg-slate-50 p-2  text-dark shadow-sm shadow-dark/50 outline-none placeholder:text-dark/70"
+            maxLength={50}
+            placeholder="Introduzca un título para esta tarjeta..."
+          >
+            {title}
+          </textarea>
+        </div>
+        <div className="mt-1 flex items-center gap-2">
+          <button
+            type="submit"
+            onClick={startCreateTodo}
+            className="cursor-pointer rounded-md bg-emerald p-2 px-4 text-sm text-white hover:bg-emerald/80"
+          >
+            Añadir tarjeta
+          </button>
+          <button onClick={() => setIsInputOpen(false)}>
+            <RiCloseFill className="text-2xl text-dark/80 hover:text-dark/50" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      onClick={() => setIsInputOpen(true)}
+      className="flex w-full items-center gap-1 rounded-md bg-gray-200 px-2 py-1 hover:bg-gray-300"
+    >
+      <RiAddLine className="text-gray-600" />
+      <span className="text-gray-600">Añada una tarjeta</span>
+    </button>
+  );
+};
