a3313f8e0a68d42f30ccb57a481c80a62ebc624c
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-08 19:14:19 -0300

    feat(client): :sparkles: add CardList component

---
+++ packages/client/components/cards/CardList.tsx
@@ -0,0 +1,71 @@
+import { useContext, DragEvent } from "react";
+
+//* components *//
+import { NewCard, Card } from ".";
+
+//* contexts *//
+import { CardContext } from "../../context";
+
+//* styles *//
+import Styles from "./TodoList.module.css";
+
+//* interfaces *//
+import { ICard, ICardStatus } from "../../interfaces";
+
+interface Props {
+  boardId: string;
+  status: ICardStatus;
+  title: string;
+  cardsProp: ICard[];
+}
+
+export const CardList: React.FC<Props> = ({
+  boardId,
+  status,
+  title,
+  cardsProp,
+}) => {
+  const { isCardDragging, onToggleCardDragging, onUpdateCard } =
+    useContext(CardContext);
+
+  const allowDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const onDropEntry = async (event: DragEvent<HTMLElement>) => {
+    const todoId = event.dataTransfer.getData("todoId");
+    await onUpdateCard({ status }, todoId, boardId);
+
+    onToggleCardDragging(false);
+  };
+
+  return (
+    <section
+      className={
+        isCardDragging
+          ? `${Styles.dragging} mx-2 mb-2 max-h-[calc(100%_-_8px)] w-[280px] min-w-[280px] overflow-y-auto`
+          : "mx-2 mb-2 max-h-[calc(100%_-_8px)] w-[280px] min-w-[280px] overflow-y-auto"
+      }
+      onDrop={onDropEntry}
+      onDragOver={allowDrop}
+    >
+      <div className="flex flex-col gap-1 rounded-md bg-slate-300 p-2">
+        <div className="mb-1 flex w-full items-center">
+          <h1 className="font-bold tracking-[1px] text-dark/60">{title}</h1>
+        </div>
+        <div className="flex flex-col gap-2">
+          {cardsProp.length > 0 && (
+            <div className="flex flex-col gap-[6px]">
+              {cardsProp.map((card) => (
+                <Card key={card.id} card={card} />
+              ))}
+            </div>
+          )}
+          <div>
+            <NewCard boardId={boardId} status={status} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
