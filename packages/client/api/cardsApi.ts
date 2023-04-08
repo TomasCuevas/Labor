e9ed295599b0d31f24f9519d08f4135c9b36b6ea
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-08 19:16:42 -0300

    refactor(client): :recycle: rename todosApi for cardsApi

--- packages/client/api/cardsApi.ts
+++ packages/client/api/cardsApi.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const todosApi = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/todos`,
+const cardsApi = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/cards`,
 });
 
-todosApi.interceptors.request.use((config) => {
+cardsApi.interceptors.request.use((config) => {
   config.headers!["Authorization"] = `Bearer ${Cookies.get("labortoken")}`;
   return config;
 });
 
-export default todosApi;
+export default cardsApi;
