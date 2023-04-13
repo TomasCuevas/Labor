72a234663541faa080fc93a317784b647bf6f516
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-12 21:39:11 -0300

    feat(client): :sparkles: add SearchInputMobile component

---
+++ packages/client/components/header/SearchInputMobile.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+//* icons *//
+import { RiSearch2Line } from "react-icons/ri";
+
+export const SearchInputMobile: React.FC = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <Link href="/search">
+      <button
+        className={`ml-auto h-full items-center rounded-md px-2 hover:bg-light/50 sm:hidden ${
+          pathname === "/search" ? "hidden" : "flex"
+        }`}
+      >
+        <RiSearch2Line className="text-xl text-white" />
+      </button>
+    </Link>
+  );
+};
