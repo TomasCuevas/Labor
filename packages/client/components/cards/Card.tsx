c4492f7aa18e652e18f4fda55b975ebe3eebc2e2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-12 21:43:28 -0300

    feat(client): :sparkles: add labels on Card component

--- packages/client/components/cards/Card.tsx
+++ packages/client/components/cards/Card.tsx
@@ -3,6 +3,9 @@ import { DragEvent, useContext } from "react";
 //* icons *//
 import { RiAlignLeft } from "react-icons/ri";
 
+//* data *//
+import { labelColors } from "../../data";
+
 //* context *//
 import { CardContext } from "../../context";
 
@@ -28,6 +31,17 @@ export const Card: React.FC<Props> = ({ card }) => {
       onClick={() => onSetCardModal(card)}
       className="flex cursor-pointer flex-col gap-[2px] rounded-md bg-gray-100 p-2 shadow-sm shadow-dark/50 hover:bg-gray-200"
     >
+      {card.labels.length > 0 ? (
+        <div className="flex flex-wrap gap-1">
+          {card.labels.map((label: string) => (
+            <span
+              className="h-[9px] w-10 rounded-full"
+              style={{ backgroundColor: labelColors[label] }}
+              key={label}
+            ></span>
+          ))}
+        </div>
+      ) : null}
       <div>
         <span className="text-sm text-gray-600">{card.title}</span>
       </div>
