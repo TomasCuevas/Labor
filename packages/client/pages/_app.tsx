7e5db3042fb8fcc2294013037a32f165e24788c2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-14 15:38:40 -0300

    refactor(client): :recycle: remove providers from _app.tsx and add auth.store

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,42 +1,35 @@
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from "notistack";
 
-//* providers *//
-import {
-  AuthProvider,
-  BoardsProvider,
-  HeaderProvider,
-  CardProvider,
-} from "../context";
+//* store *//
+import { useAuthStore } from "../store";
 
 //* styles *//
 import "../styles/globals.css";
 
 //* create a client *//
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
+
+//* snackbar provider *//
+export const snackbarProvider = React.createRef<SnackbarProvider>();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { onCheckAuthentication } = useAuthStore();
+
+  useEffect(() => {
+    onCheckAuthentication();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <SnackbarProvider maxSnack={3}>
-        <AuthProvider>
-          <BoardsProvider>
-            <CardProvider>
-              <HeaderProvider>
-                <Head>
-                  <link
-                    rel="shortcut icon"
-                    href="/labor.svg"
-                    type="image/x-icon"
-                  />
-                </Head>
-                <Component {...pageProps} />
-              </HeaderProvider>
-            </CardProvider>
-          </BoardsProvider>
-        </AuthProvider>
+      <SnackbarProvider maxSnack={3} ref={snackbarProvider}>
+        <Head>
+          <link rel="shortcut icon" href="/labor.svg" type="image/x-icon" />
+        </Head>
+        <Component {...pageProps} />
       </SnackbarProvider>
     </QueryClientProvider>
   );
