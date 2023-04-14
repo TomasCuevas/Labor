9c723097500a438a0eaf676aba08ea5752781f72
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-14 15:45:28 -0300

    feat(client): :sparkles: add notistack utils

---
+++ packages/client/utils/notistack.utils.ts
@@ -0,0 +1,23 @@
+import { snackbarProvider } from "../pages/_app";
+
+export const notiSuccess = (menssaje: string) => {
+  snackbarProvider.current?.enqueueSnackbar(menssaje, {
+    variant: "success",
+    autoHideDuration: 3000,
+    anchorOrigin: {
+      vertical: "top",
+      horizontal: "right",
+    },
+  });
+};
+
+export const notiError = (menssaje: string) => {
+  snackbarProvider.current?.enqueueSnackbar(menssaje, {
+    variant: "error",
+    autoHideDuration: 3000,
+    anchorOrigin: {
+      vertical: "top",
+      horizontal: "right",
+    },
+  });
+};
