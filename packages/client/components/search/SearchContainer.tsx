18ea49e5a2158178a96c3e94a224b9407381085d
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-17 21:23:42 -0300

    style(client): :lipstick: add backdrop-blur filter on SearchContainer component

--- packages/client/components/search/SearchContainer.tsx
+++ packages/client/components/search/SearchContainer.tsx
@@ -34,44 +34,45 @@ export const SearchContainer: React.FC = () => {
   }, [search]);
 
   return (
-    <section className="mx-auto mt-4 w-full max-w-[680px] px-4 sm:px-8 md:px-0">
-      <h2 className="text-2xl font-black text-light">Buscar</h2>
-      <div className="relative mt-4 flex items-center">
-        <RiCloseFill
-          className="absolute right-0 mr-2 text-2xl text-black/70"
-          onClick={reset}
-        />
-        <input
-          autoFocus
-          autoComplete="off"
-          className="w-full rounded-md border-none p-3 pr-8 text-sm outline-none"
-          name="search"
-          onChange={onInputChange}
-          placeholder="Introduzca su palabra clave de búsqueda aquí"
-          type="text"
-          value={search}
-        />
-      </div>
-      {cards!.length > 0 && (
-        <div className="mt-3">
-          <h3 className="text-[17px] font-bold text-light">Tarjetas</h3>
-          <div className="flex flex-col pt-1">
-            {cards!.map((card) => (
-              <SearchCard key={card.id} card={card} />
-            ))}
-          </div>
+    <div className="mx-4 mt-4 flex w-full items-start">
+      <section className="mx-auto flex max-h-[calc(100vh_-_64px)] w-full max-w-[750px] flex-col overflow-y-scroll rounded-2xl bg-dark/20 px-4 py-4 backdrop-blur-2xl sm:px-8">
+        <h2 className="text-2xl font-black text-white">Buscar</h2>
+        <div className="relative mt-4 flex items-center">
+          <button className="absolute right-0 mr-2">
+            <RiCloseFill className="text-2xl text-black/70" onClick={reset} />
+          </button>
+          <input
+            autoFocus
+            autoComplete="off"
+            className="w-full rounded-md border-none p-3 pr-8 text-sm outline-none"
+            name="search"
+            onChange={onInputChange}
+            placeholder="Introduzca su palabra clave de búsqueda aquí"
+            type="text"
+            value={search}
+          />
         </div>
-      )}
-      {boards!.length > 0 && (
-        <div className="mt-3">
-          <h3 className="text-[17px] font-bold text-light">Tableros</h3>
-          <div className="flex flex-col pt-1">
-            {boards!.map((board) => (
-              <SearchBoard key={board.id} board={board} />
-            ))}
+        {cards!.length > 0 && (
+          <div className="mt-3">
+            <h3 className="text-[17px] font-bold text-white">Tarjetas</h3>
+            <div className="flex flex-col pt-1">
+              {cards!.map((card) => (
+                <SearchCard key={card.id} card={card} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </section>
+        )}
+        {boards!.length > 0 && (
+          <div className="mt-3">
+            <h3 className="text-[17px] font-bold text-white">Tableros</h3>
+            <div className="flex flex-col pt-1">
+              {boards!.map((board) => (
+                <SearchBoard key={board.id} board={board} />
+              ))}
+            </div>
+          </div>
+        )}
+      </section>
+    </div>
   );
 };
