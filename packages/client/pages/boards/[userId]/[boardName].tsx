aef28f8a51e691f01c890c8f9e33aaa475fa63a7
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-18 23:00:16 -0300

    refactor(client): :recycle: replace CardContext for useCardsStore on [boardName] page

--- packages/client/pages/boards/[userId]/[boardName].tsx
+++ packages/client/pages/boards/[userId]/[boardName].tsx
@@ -1,16 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { NextPage, GetServerSideProps } from "next";
 import axios from "axios";
 
 //* components *//
-import { Board } from "../../../components/boards";
+import { Board, BoardHeader, BoardSidebar } from "../../../components/boards";
 import { CardModal } from "../../../components/cards";
 
 //* layout *//
 import { MainLayout } from "../../../layouts";
 
-//* context *//
-import { CardContext } from "../../../context";
+//* store *//
+import { useBoardInterfaceStore, useCardsStore } from "../../../store";
 
 //* interfaces *//
 import { IBoard, ICard } from "../../../interfaces";
@@ -21,11 +21,12 @@ interface Props {
 }
 
 const BoardPage: NextPage<Props> = ({ board, modalCard }) => {
-  const { cardModal, onSetCardModal } = useContext(CardContext);
+  const { cardModal, onToggleCardModal } = useCardsStore();
+  const { onSetBoard, sidebar } = useBoardInterfaceStore();
 
   useEffect(() => {
-    if (!modalCard) return;
-    if (modalCard) onSetCardModal(modalCard);
+    if (modalCard) onToggleCardModal(modalCard);
+    onSetBoard(board);
   }, []);
 
   return (
@@ -33,8 +34,17 @@ const BoardPage: NextPage<Props> = ({ board, modalCard }) => {
       title={`${board.name} | Labor`}
       description={`Pagina dedicada al tablero ${board.name} creado por ${board.user.name}`}
     >
-      <Board boardId={board.id} />
-      {cardModal && <CardModal />}
+      <div
+        className="flex h-full w-full flex-col bg-cover bg-center bg-no-repeat pt-11"
+        style={{
+          backgroundImage: `url(/board_background/${board.background}.svg)`,
+        }}
+      >
+        <BoardHeader boardName={board.name} />
+        <Board boardId={board.id} />
+      </div>
+      {cardModal ? <CardModal /> : null}
+      {sidebar ? <BoardSidebar /> : null}
     </MainLayout>
   );
 };
