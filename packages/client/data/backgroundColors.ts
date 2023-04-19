e11994051c4dcfe65320aadf82009f0926f6d2c8
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-18 23:02:34 -0300

    chore(client): :sparkles: add backgroundColors data

---
+++ packages/client/data/backgroundColors.ts
@@ -0,0 +1,11 @@
+export const backgroundColors = {
+  blue: "#08479e",
+  emerald: "#14553a",
+  gray: "#46536a",
+  lightblue: "#08479e",
+  lightpurple: "#473699",
+  maroon: "#2e1c0a",
+  orange: "#872013",
+  pink: "#821659",
+  purple: "#072754",
+};
--- packages/client/data/index.ts
+++ packages/client/data/index.ts
@@ -1,1 +1,2 @@
+export * from "./backgroundColors";
 export * from "./labelColors";
