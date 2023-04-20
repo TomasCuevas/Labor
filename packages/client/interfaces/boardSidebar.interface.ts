5df7a050b3110ac4b0c70fd7be97cb9339d12fdb
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-19 23:56:21 -0300

    chore(client): :label: add boardSidebar.interface file

---
+++ packages/client/interfaces/boardSidebar.interface.ts
@@ -0,0 +1,1 @@
+export type IBoardSidebarSection = "main" | "about" | "background";
--- packages/client/interfaces/index.ts
+++ packages/client/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth.interfaces";
 export * from "./board.interfaces";
+export * from "./boardSidebar.interface";
 export * from "./card.interfaces";
 export * from "./header.interfaces";
 export * from "./search.interfaces";
