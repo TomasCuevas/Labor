f6ac13d6dd1633ce38642130608ff66ec7d798e9
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-20 21:45:09 -0300

    feat(client): :sparkles: add BoardSidebarItem component

---
+++ packages/client/components/boards/sidebar/BoardSidebarItem.tsx
@@ -0,0 +1,36 @@
+//* store *//
+import { useBoardInterfaceStore } from "../../../store";
+
+//* interface *//
+import { IBoardSidebarSection } from "../../../interfaces";
+
+interface Props {
+  description?: string;
+  icon?: React.ReactNode;
+  navigate?: IBoardSidebarSection;
+  title: string;
+}
+
+export const BoardSidebarItem: React.FC<Props> = ({
+  description,
+  icon,
+  navigate,
+  title,
+}) => {
+  const { onSetSidebarSection } = useBoardInterfaceStore();
+
+  return (
+    <li
+      onClick={navigate ? () => onSetSidebarSection(navigate) : () => {}}
+      className="flex w-full cursor-pointer items-center gap-4 rounded-md px-3 py-2 hover:bg-gray-300"
+    >
+      {icon ? <div className="w-6">{icon}</div> : null}
+      <div className="flex flex-col">
+        <span className="text-[13px] font-medium">{title}</span>
+        {description ? (
+          <span className="text-[13px] font-light">{description}</span>
+        ) : null}
+      </div>
+    </li>
+  );
+};
