1c09de41dbb75dd1e05e65309c46eb163e67f359
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-20 21:44:36 -0300

    feat(client): :sparkles: add BoardSidebar component

---
+++ packages/client/components/boards/sidebar/BoardSidebar.tsx
@@ -0,0 +1,17 @@
+//* components *//
+import { AboutSection, BackgroundSection, MainSection } from "../";
+
+//* store *//
+import { useBoardInterfaceStore } from "../../../store";
+
+export const BoardSidebar: React.FC = () => {
+  const { sidebarSection } = useBoardInterfaceStore();
+
+  return (
+    <aside className="absolute right-0 top-[44px] flex h-[calc(100vh_-_44px)] w-[340px] flex-col items-center gap-4 bg-gray-200/40 px-3 backdrop-blur-2xl">
+      {sidebarSection === "main" ? <MainSection /> : null}
+      {sidebarSection === "about" ? <AboutSection /> : null}
+      {sidebarSection === "background" ? <BackgroundSection /> : null}
+    </aside>
+  );
+};
