18ca63b8608087ffd68965be6b73c26aa3ce466f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-20 23:05:49 -0300

    feat(client): :sparkles: add BackgroundCard component

---
+++ packages/client/components/boards/sidebar/items/BackgroundCard.tsx
@@ -0,0 +1,35 @@
+import { MouseEvent } from "react";
+
+//* icons *//
+import { RiCheckFill } from "react-icons/ri";
+
+//* store *//
+import { useBoardInterfaceStore, useBoardsStore } from "../../../../store";
+
+//* interface *//
+interface Props {
+  background: string;
+  isSelected: boolean;
+}
+
+export const BackgroundCard: React.FC<Props> = ({ background, isSelected }) => {
+  const { onUpdateBoard } = useBoardsStore();
+  const { board, onSetBoard } = useBoardInterfaceStore();
+
+  //! start update background
+  const startUpdateBackground = async () => {
+    const result = await onUpdateBoard(board!.id, { background });
+    if (result) onSetBoard(result.board!);
+  };
+
+  return (
+    <div
+      onClick={() => startUpdateBackground()}
+      className="relative flex h-24 w-full cursor-pointer items-center justify-center overflow-hidden rounded-xl bg-cover bg-center"
+      style={{ backgroundImage: `url(/board_background/${background}.svg)` }}
+    >
+      <div className="absolute left-0 top-0 h-full w-full hover:bg-white/40"></div>
+      {isSelected ? <RiCheckFill className="text-2xl text-white" /> : null}
+    </div>
+  );
+};
