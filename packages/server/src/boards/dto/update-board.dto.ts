1824b2875aa0fb135963073efafcd5289e6a8c45
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-02 22:52:38 -0300

    feat(server): :sparkles: add status property on UpdateBoard DTO

--- packages/server/src/boards/dto/update-board.dto.ts
+++ packages/server/src/boards/dto/update-board.dto.ts
@@ -1,6 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsIn, IsOptional, IsString } from 'class-validator';
 
 //* dto *//
 import { CreateBoardDto } from './';
 
-export class UpdateBoardDto extends PartialType(CreateBoardDto) {}
+export class UpdateBoardDto extends PartialType(CreateBoardDto) {
+  @IsString()
+  @IsIn(['open', 'closed'])
+  @IsOptional()
+  status?: 'open' | 'closed';
+}
