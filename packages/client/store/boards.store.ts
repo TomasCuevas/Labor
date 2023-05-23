23eba4c585d90bdb9299f256afa3312f51d373a2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-22 23:56:40 -0300

    refactor(client): :recycle: add try-catch on board store methods

--- packages/client/store/boards.store.ts
+++ packages/client/store/boards.store.ts
@@ -17,59 +17,43 @@ import {
 import { IBoard, IBoardForCreate, IBoardForUpdate } from "@/interfaces";
 
 interface useBoardsState {
-  onCreateBoard(
-    board: IBoardForCreate
-  ): Promise<{ ok: boolean; board?: IBoard }>;
-  onUpdateBoard(
-    boardId: string,
-    board: IBoardForUpdate
-  ): Promise<{ ok: boolean; board?: IBoard }>;
-  onDeleteBoard(boardId: string): Promise<{ ok: boolean }>;
+  onCreateBoard(board: IBoardForCreate): Promise<IBoard>;
+  onUpdateBoard(boardId: string, board: IBoardForUpdate): Promise<IBoard>;
+  onDeleteBoard(boardId: string): Promise<void>;
 }
 
 export const useBoardsStore = create<useBoardsState>(() => ({
-  async onCreateBoard(board: IBoardForCreate) {
-    const result = await createBoardService(board);
-    if (result.ok) {
+  async onCreateBoard(boardData: IBoardForCreate) {
+    try {
+      const board = await createBoardService(boardData);
       queryClient.invalidateQueries(["/boards"]);
-      return {
-        ok: true,
-        board: result.board,
-      };
-    } else {
-      return {
-        ok: false,
-      };
+      return board;
+    } catch (error: any) {
+      notiError(error.response.data.message[0] || "Error al crear el tablero.");
+      throw error;
     }
   },
-  async onUpdateBoard(boardId: string, board: IBoardForUpdate) {
-    const result = await updateBoardService(boardId, board);
-    if (result.ok) {
+  async onUpdateBoard(boardId: string, boardData: IBoardForUpdate) {
+    try {
+      const board = await updateBoardService(boardId, boardData);
       queryClient.invalidateQueries(["/boards"]);
-      return {
-        ok: true,
-        board: result.board,
-      };
-    } else {
-      notiError("Error al actualizar el tablero.");
-      return {
-        ok: false,
-      };
+      return board;
+    } catch (error: any) {
+      notiError(
+        error.response.data.message[0] || "Error al actualizar el tablero."
+      );
+      throw error;
     }
   },
   async onDeleteBoard(boardId: string) {
-    const result = await deleteBoard(boardId);
-
-    if (result.ok) {
+    try {
+      await deleteBoard(boardId);
       queryClient.invalidateQueries(["/boards"]);
-      return {
-        ok: true,
-      };
-    } else {
-      notiError("Error al eliminar el tablero.");
-      return {
-        ok: false,
-      };
+    } catch (error: any) {
+      notiError(
+        error.response.data.message[0] || "Error al eliminar el tablero."
+      );
+      throw error;
     }
   },
 }));
