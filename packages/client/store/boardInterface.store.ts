eb30045acbb3ce40ea156ba3be11573244f489f6
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-22 23:50:06 -0300

    feat(client): :necktie: add new property and method on boardInterface store

--- packages/client/store/boardInterface.store.ts
+++ packages/client/store/boardInterface.store.ts
@@ -4,32 +4,31 @@ import { create } from "zustand";
 import { IBoard, IBoardSidebarSection } from "@/interfaces";
 
 interface useBoardInterfaceState {
+  closedBoardsModal: boolean;
   sidebar: boolean;
   sidebarSection: IBoardSidebarSection;
   board?: IBoard;
+  onToggleClosedBoardsModal(value: boolean): void;
   onSetSidebarSection(section: IBoardSidebarSection): void;
   onSetBoard(board: IBoard): void;
   onToggleSidebar(value: boolean): void;
 }
 
 export const useBoardInterfaceStore = create<useBoardInterfaceState>((set) => ({
+  closedBoardsModal: false,
   sidebar: false,
   sidebarSection: "main",
   board: undefined,
-  onSetSidebarSection(section: IBoardSidebarSection) {
-    set(() => ({
-      sidebarSection: section,
-    }));
+  onToggleClosedBoardsModal(value: boolean) {
+    set(() => ({ closedBoardsModal: value }));
   },
   onSetBoard(board: IBoard) {
-    set(() => ({
-      board,
-    }));
+    set(() => ({ board }));
+  },
+  onSetSidebarSection(section: IBoardSidebarSection) {
+    set(() => ({ sidebarSection: section }));
   },
   onToggleSidebar(value: boolean) {
-    set(() => ({
-      sidebar: value,
-      sidebarSection: "main",
-    }));
+    set(() => ({ sidebar: value, sidebarSection: "main" }));
   },
 }));
