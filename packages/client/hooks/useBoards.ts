98a8cea8caa6395c11f368610f1f3889d50e05c6
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-23 04:37:26 -0300

    feat(client): :necktie: add closedBoards property on useBoards hook

--- packages/client/hooks/useBoards.ts
+++ packages/client/hooks/useBoards.ts
@@ -1,23 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 
 //* service *//
-import { getAllBoardsService } from "@/services";
+import { getAllClosedBoards, getAllOpenBoardsService } from "@/services";
 
 //* interfaces *//
 import { IBoard } from "@/interfaces";
 
 interface Return {
-  boards?: IBoard[];
+  boards: IBoard[];
+  closedBoards: IBoard[];
   isLoading: boolean;
 }
 
 export const useBoards = (): Return => {
-  const { data: boards, isLoading } = useQuery(["/boards"], () =>
-    getAllBoardsService()
+  const { data: boards = [], isLoading } = useQuery(["/boards"], () =>
+    getAllOpenBoardsService()
+  );
+  const { data: closedBoards = [] } = useQuery(["/boards/closed"], () =>
+    getAllClosedBoards()
   );
 
   return {
     boards,
+    closedBoards,
     isLoading,
   };
 };
