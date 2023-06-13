a5ec0847aa944414359a36d3721b707e660dcfc8
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-13 13:47:26 -0300

    feat(server): :sparkles: add origins cors on server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,9 +4,14 @@ import { NestFactory } from '@nestjs/core';
 //* principal module *//
 import { AppModule } from '@/app.module';
 
+//* origines permitidos por cors
+const origins = process.env.ORIGINS.split(' ');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: origins,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
