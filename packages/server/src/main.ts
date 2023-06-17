4221ceab343999302d8309751e0a6f0a9c124559
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-17 12:01:29 -0300

    fix(server): :bug: add cors config on Nest

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app.module';
 const origins = process.env.ORIGINS.split(' ');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
